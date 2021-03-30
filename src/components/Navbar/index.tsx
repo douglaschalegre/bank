eb9487a04f4c064ce8ffa91f1dcbb62dc466e52e
Douglas Chalegre <douglas.chalegre@gmail.com>
2021-03-30 01:09:51 -0300

    Creating and styling navbar styled components

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import {Nav, NavbarContainer, NavLogo, Drawer, NavMenu, NavItem, NavLink, NavBtn, NavBtnLink} from './navbarElements'
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <>
+          <Nav>
+            <NavbarContainer>
+              <NavLogo to='/'>vBank</NavLogo>
+              <Drawer>
+                <FaBars/>
+              </Drawer>
+              <NavMenu>
+                <NavItem>
+                  <NavLink to="about">About</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="discover">Discover</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="services">Services</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="signup">Sign Up</NavLink>
+                </NavItem>
+              </NavMenu>
+              <NavBtn>
+                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+              </NavBtn>
+            </NavbarContainer>
+          </Nav>
+        </>
     )
 }
 
